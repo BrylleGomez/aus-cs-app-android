8078f1e15c563072ad672c75c786fc9c9ac4c754
Brylle Gomez <brylle.gomez@gmail.com>
2019-11-16 12:57:52 +0400

    Initial commit

---
+++ app/src/main/java/com/brylle/aus_cs_app_android_j/MainActivity.java
@@ -0,0 +1,14 @@
+package com.brylle.aus_cs_app_android_j;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
