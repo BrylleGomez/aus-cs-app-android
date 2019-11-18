4ed4259bc8f1f35b600223b38b05d5a56c55fba0
Brylle Gomez <brylle.gomez@gmail.com>
2019-11-18 19:05:36 +0400

    Recycler View UI Working (without firestore)

--- app/src/main/java/com/brylle/aus_cs_app_android_j/events/EventAdapter.java
+++ app/src/main/java/com/brylle/aus_cs_app_android_j/events/EventAdapter.java
@@ -1,47 +1,83 @@
 package com.brylle.aus_cs_app_android_j.events;
 
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+
+import com.brylle.aus_cs_app_android_j.R;
 
 import java.util.ArrayList;
 
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    public interface OnItemClickListener {
+        void onItemClick(Event event);
+    }
+
     // declaring some fields.
     private ArrayList<Event> list;
-    public EventAdapter(ArrayList<Event> arrayList) {
+    private OnItemClickListener clickListener;
+
+    public EventAdapter(ArrayList<Event> arrayList, OnItemClickListener clickListener) {
         this.list = arrayList;
+        this.clickListener = clickListener;
     }
 
     @NonNull
     @Override
     public EventAdapter.EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_events_event_viewholder,parent,false);
+
+        return new EventViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull EventAdapter.EventViewHolder holder, int position) {
-
+        holder.bind(list.get(position), clickListener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     // Inner ViewHolder Class for Event
     public class EventViewHolder extends RecyclerView.ViewHolder {
         TextView nameView, dateView, timeView;
+        Button button;
 
         public EventViewHolder(View itemView) {
             super(itemView);
-            Log.v("ViewHolder","in View Holder");
-            name = itemView.findViewById(R.id.textView);
-            number = itemView.findViewById(R.id.textView2);
-            addedOn = itemView.findViewById(R.id.textView3);
+            nameView = itemView.findViewById(R.id.fragment_events_event_viewholder_name);
+            dateView = itemView.findViewById(R.id.fragment_events_event_viewholder_date);
+            timeView = itemView.findViewById(R.id.fragment_events_event_viewholder_time);
+            button = itemView.findViewById(R.id.fragment_events_event_viewholder_register);  // test
         }
+
+        public void bind(final Event event, final OnItemClickListener clickListener) {
+            nameView.setText(event.getName());
+            dateView.setText(event.getDates());
+            timeView.setText(event.getTimes());
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    clickListener.onItemClick(event);
+                }
+            });
+        }
+
     }
 
 }
+
+//Event event = list.get(position);
+//holder.nameView.setText(event.getName());
+//holder.dateView.setText(event.getDates());
+//holder.timeView.setText(event.getTimes());
--- app/src/main/java/com/brylle/aus_cs_app_android_j/events/EventsFragment.java
+++ app/src/main/java/com/brylle/aus_cs_app_android_j/events/EventsFragment.java
@@ -3,21 +3,48 @@ package com.brylle.aus_cs_app_android_j.events;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.brylle.aus_cs_app_android_j.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class EventsFragment extends Fragment {
 
+    /* Variables */
+
+    private ArrayList<Event> eventsList = new ArrayList<>();
+    private RecyclerView eventsView;
+    private EventAdapter eventsAdapter;
+    private FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private CollectionReference firestoreUserList = firebaseFirestore.collection("users");
+    private CollectionReference firestoreEventList = firebaseFirestore.collection("events");
+
+    /* Initializer Functions */
 
     public EventsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,5 +52,67 @@ public class EventsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_events, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize Objects
+        eventsView = Objects.requireNonNull(getView()).findViewById(R.id.events_recyclerview);
+
+        // Fetches all event database entries and stores them in a list of event objects
+        firebaseFirestore.collection("events")
+            .get()                                  // Fetch all event entries from database
+            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                    for (DocumentSnapshot document : queryDocumentSnapshots) {      // Iterate through all fetched events
+                        // Store info of each fetched event in temp variable
+                        @NonNull int eventId = document.getLong("event_id").intValue();
+                        String eventName = document.getString("event_name");
+                        String startDate = document.getString("start_date");
+                        String endDate = document.getString("end_date");
+                        String startTime = document.getString("start_time");
+                        String endTime = document.getString("end_time");
+                        // Create an Event object with the retrieved event info (in temp variables)
+                        // Add created Event object to the container
+                        eventsList.add(
+                                new Event(
+                                        eventId,
+                                        eventName,
+                                        startDate,
+                                        endDate,
+                                        startTime,
+                                        endTime
+                                )
+                        );
+                        Log.d("EventsFragment", document.getString("event_name") + "added!");
+                    }
+
+                    // set up recycler view
+                    eventsAdapter = new EventAdapter(eventsList, new EventAdapter.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Event event) {
+                            // Adds a user to an event's list of registered students
+                            // and adds an event to a user's list of registered events
+                            // when the register button of each event is clicked
+                            Toast.makeText(getContext(), "Retrieving event " + event.getID(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    eventsView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    eventsView.setAdapter(eventsAdapter);
+                }
+
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("EventsFragment", "Error getting documents: ", e);
+                }
+            });
+
+    }
+
+    /* Helper Functions */
 
 }
