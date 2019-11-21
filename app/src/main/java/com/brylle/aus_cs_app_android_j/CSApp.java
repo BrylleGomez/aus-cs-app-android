16667a890f8d1b41e9d2b42b353ba1bbeb83c0c7
Brylle Gomez <brylle.gomez@gmail.com>
2019-11-21 16:05:08 +0400

    Revert "Notifications for new event functional"
    This reverts commit 5205e032d774358a36401d84a1c1aacbfa9dd2ee.

--- app/src/main/java/com/brylle/aus_cs_app_android_j/CSApp.java
+++ app/src/main/java/com/brylle/aus_cs_app_android_j/CSApp.java
@@ -1,19 +1,4 @@
 package com.brylle.aus_cs_app_android_j;
 
-import android.app.Application;
-import android.content.Intent;
-import android.util.Log;
-
-public class CSApp extends Application {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.d("CSApp", "AUS CS App started!");
-
-        // start service
-        Intent service = new Intent(this, CSService.class);
-        startService(service);
-    }
-
+public class CSApp {
 }
--- app/src/main/java/com/brylle/aus_cs_app_android_j/events/EventsFragment.java
+++ app/src/main/java/com/brylle/aus_cs_app_android_j/events/EventsFragment.java
@@ -23,11 +23,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
