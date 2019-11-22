0cb80ad0385218f2ca8a1a424c537e57efd24a67
Brylle Gomez <brylle.gomez@gmail.com>
2019-11-22 17:34:30 +0400

    Fixed event details activity, register, and maps

---
+++ app/src/main/java/com/brylle/aus_cs_app_android_j/profile/RegEventDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.brylle.aus_cs_app_android_j.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegEventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_event_details);
+    }
+}
